a568086ee7b8477b10b644c56445676e573d37b4
bix-1 <jbartkoj@gmail.com>
2021-10-29 18:35:42 +0200

    Addex data extraction for plotting

--- get_stat.py
+++ get_stat.py
@@ -9,7 +9,33 @@ from download import DataDownloader
 def plot_stat(data_source,
               fig_location=None,
               show_figure=False):
-    pass
+    print()
+
+    type_strs = ["Žiadna úprava", "Prerušovaná žltá", "Semafor mimo prevádzky",
+                 "Dopravné značky", "Prenosné dopravné značky", "Nevyznačená"]
+
+    data, regions = get_data(data_source)
+
+
+
+def get_data(src):
+    out = []
+    regions, counts = np.unique(src["region"], return_counts=1)
+    index = 0
+    pos = 0
+    N = 6
+    for c in counts:
+        types, sums = np.unique(src["p24"][pos:pos+c], return_counts=1)
+        result = [0] * N
+        for i in range(len(types)):
+            result[types[i]] = sums[i]
+
+        out.append(result)
+
+        pos += c
+        index += 1
+
+    return out, regions
 
 
 # TODO pri spusteni zpracovat argumenty
