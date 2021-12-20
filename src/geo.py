c225057f645f854f41d14f127d809966fbaa23ab
bix-1 <jbartkoj@gmail.com>
2021-12-20 02:51:42 +0100

    Added plot geo

--- src/geo.py
+++ src/geo.py
@@ -3,29 +3,84 @@
 import pandas as pd
 import geopandas
 import matplotlib.pyplot as plt
-import contextily
+import contextily as ctx
 import sklearn.cluster
 import numpy as np
-# muzete pridat vlastni knihovny
+import sys
+import os
+
+
+def _save_fig(fig_location):
+    """
+    Saves current PyPlot figure to specified location
+
+    Parameters
+    ----------
+    fig_location: str
+        path for figure storing
+    """
+
+    try:
+        dir = os.path.dirname(fig_location)
+        if dir and not os.path.exists(dir):
+            os.makedirs(dir)
+        plt.savefig(fig_location)
+    except:
+        print("ERROR: Failed to save figure", fig_location, file=sys.stderr)
+        sys.exit(1)
 
 
 def make_geo(df: pd.DataFrame) -> geopandas.GeoDataFrame:
-    """ Konvertovani dataframe do geopandas.GeoDataFrame se spravnym kodovani"""
-    pass
+    df["date"] = pd.to_datetime(df["p2a"])
+    df = df.loc[df["e"].notnull() & df["d"].notnull()]
+    return geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df["d"], df["e"]), crs="EPSG:5514")
+
 
 def plot_geo(gdf: geopandas.GeoDataFrame, fig_location: str = None,
              show_figure: bool = False):
-    """ Vykresleni grafu s sesti podgrafy podle lokality nehody 
+    """ Vykresleni grafu s sesti podgrafy podle lokality nehody
      (dalnice vs prvni trida) pro roky 2018-2020 """
-    pass
+
+    gdf = gdf[(gdf["region"] == "JHM") & (gdf["p36"] <= 1) &
+              (gdf["date"].dt.year <= 2020)].to_crs("EPSG:3857")
+
+    rows, cols = 3, 2
+    fig, axs = plt.subplots(rows, cols, figsize=(15, 10))
+
+    _, miny, maxx, maxy = gdf.total_bounds
+    minx = 1750000.0    # one entry was assigned the wrong region
+
+    colors = ["tab:red", "tab:blue"]
+
+    for r in range(rows):
+        for c in range(cols):
+            axs[r, c].set_title("JHM Region: {} ({})".format(
+                "highway" if c == 0 else "1st class road", 2018 + r))
+            axs[r, c].axis("off")
+            axs[r, c].set_xlim(minx, maxx)
+            axs[r, c].set_ylim(miny, maxy)
+
+            gdf[(gdf["p36"] == c) & (gdf["date"].dt.year == (2018 + r))
+                ].plot(ax=axs[r, c], markersize=2, color=colors[c])
+            ctx.add_basemap(axs[r, c], crs=gdf.crs.to_string(), source=ctx.providers.Stamen.TonerLite,
+                            alpha=0.9, zoom=10)
+
+    plt.tight_layout()
+
+    # showing / storing figure
+    if fig_location:
+        _save_fig(fig_location)
+    if show_figure:
+        plt.show()
+
 
 def plot_cluster(gdf: geopandas.GeoDataFrame, fig_location: str = None,
                  show_figure: bool = False):
     """ Vykresleni grafu s lokalitou vsech nehod v kraji shlukovanych do clusteru """
     pass
 
+
 if __name__ == "__main__":
-    # zde muzete delat libovolne modifikace
-    gdf = make_geo(pd.read_pickle("accidents.pkl.gz"))
-    plot_geo(gdf, "geo1.png", True)
-    plot_cluster(gdf, "geo2.png", True)
+    gdf = make_geo(pd.read_pickle("../data/accidents.pkl.gz"))
+    plot_geo(gdf, "geo1.pdf", False)
+    plot_cluster(gdf, "geo2.pdf", True)
