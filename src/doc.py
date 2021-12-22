4f73166809ef108aee9ffe505e6d33845bd1532d
bix-1 <jbartkoj@gmail.com>
2021-12-22 15:05:25 +0100

    Added doc plot

--- src/doc.py
+++ src/doc.py
@@ -6,9 +6,6 @@ import seaborn as sns
 import sys
 import os
 
-# TODO:
-# vehicle type: ammount of deaths vs chance of fatality
-
 
 def plot_injuries(df: pd.DataFrame, fig_location: str = None, show_figure: bool = False):
     # get data
@@ -31,17 +28,44 @@ def plot_injuries(df: pd.DataFrame, fig_location: str = None, show_figure: bool 
     fig, axs = plt.subplots(2, 1, figsize=(12, 8),
                             gridspec_kw={'height_ratios': [2, 1]})
     for ax in axs:
-        ax.set_yscale("log")
         ax.xaxis.label.set_visible(False)
+    axs[0].set_yscale("log")
 
     # plot by vehicle type
     dt1 = data.groupby(["p44"]).sum().loc[[
         "car", "truck", "motorcycle", "bus", "train"]]
     dt1.plot.bar(ax=axs[0], rot=0,
-                 colormap="autumn", title="Seriousness of accident injuries")
-    # plot total count
-    data.value_counts("p44").plot.bar(ax=axs[1], rot=0, color="#6c17bd", title="Accidents by vehicle type")
+                 colormap="autumn", title="Seriousness of injuries")
+    axs[0].set_ylabel("accidents")
+    axs[0].grid(visible=False, axis="x")
 
+    # plot total count
+    plt.style.use("seaborn-dark")
+    counts = data.value_counts("p44")
+    counts.plot.bar(
+        ax=axs[1], rot=0, color="#6c17bd", title="Lives threatened per accident")
+    axs[1].set_ylabel("accidents", color="#6c17bd")
+    axs[1].tick_params(axis='y', labelcolor="#6c17bd")
+    axs[1].grid(True)
+    axs[1].yaxis.set_ticks(axs[1].get_yticks())  # FixedLocator warning
+    yticks = ['{:.0f}'.format(x) + 'K' for x in axs[1].get_yticks()/1000]
+    axs[1].set_yticklabels(yticks)
+
+    # plot probability of injury
+    ax3 = axs[1].twinx()
+    ax3.set_ylabel("threatened", color="#DC143C")
+    ax3.tick_params(axis='y', labelcolor="#DC143C")
+    dt2 = (dt1["Deaths"] + dt1["Severely injured"]) / data.value_counts("p44")
+    dt2.plot.bar(ax=ax3, color="#DC143C", width=0.2)
+    xlocs, _ = plt.xticks()
+    for index, value in enumerate(dt2.values.tolist()):
+        plt.text(xlocs[index] - 0.12, value + 0.01,
+                 "{:,.2f}%".format(value), fontsize=12, fontweight="bold")
+    plt.text(xlocs[-1] - 0.25, 0.025, str(counts.at["train"]),
+             fontsize=14, fontweight="bold", color="#6c17bd")
+    plt.plot([xlocs[-1] - 0.18, xlocs[-1]], [0.018, 0], color="#6c17bd", lw=3)
+
+    # figure storing / showing
     if fig_location:
         fig.savefig(fig_location)
     if show_figure:
